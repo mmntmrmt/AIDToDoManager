74567fa8718bdd3872ccf28e0690dbfa9adab12d
Ryo Morimoto <morimoto@M-3.local>
2015-07-24 19:04:33 +0900

    add views to AddTodoViewController

--- AIDToDoManager/AddToDoViewController.swift
+++ AIDToDoManager/AddToDoViewController.swift
@@ -10,10 +10,25 @@ import UIKit
 
 class AddToDoViewController: UIViewController {
 
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var deadLineTextField: UITextField!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func addButtonTouchUpInside(sender: UIButton) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func cancelButtonTouchUpInside(sender: UIButton) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
 }
